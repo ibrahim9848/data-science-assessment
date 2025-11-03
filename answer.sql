c709b046420e7586c7a3731be32642569bc039ff
ibrahim Omar alghamdi <ibrahim.omar.alghamdi@gmail.com>
2025-11-03 18:23:33 +0300

    Implement share percentage calculation by sector
    Added SQL query to calculate share percentage by sector.

--- answer.sql
+++ answer.sql
@@ -1,1 +1,15 @@
 -- ADD YOUR SQL QUERY HERE
+-- Percentage of each investor's shares by sector, rounded to 2 decimals
+SELECT
+  it.investor_id,
+  s.sector_name,
+  ROUND(
+    100.0 * SUM(it.no_of_shares)
+    / SUM(SUM(it.no_of_shares)) OVER (PARTITION BY it.investor_id),
+    2
+  ) AS share_percentage
+FROM investor_transactions AS it
+JOIN sectors AS s
+  ON s.sector_id = it.sector_id
+GROUP BY it.investor_id, s.sector_name
+ORDER BY it.investor_id, share_percentage DESC;
